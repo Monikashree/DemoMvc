ecd5f857173949294a8e36135d8739dc2a261b10
ELCOT <ELCOT@LENOVO>
2020-02-22 21:01:46 -0800

    Add project files.

---
+++ OnlineTrainTicketBookingMVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using OnlineTrainTicketBooking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineTrainTicketBookingMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        UserRepository userRepository = new UserRepository() ;
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            IEnumerable<User> user = UserRepository.ShowDetails();
+            ViewBag.details = user;
+            ViewData["details"] = user;
+            TempData["details"] = user;
+            return View();
+        }
+        [HttpGet]
+        [ActionName("SignUp")]
+        public ActionResult SignUp_Get()
+        {            
+            return View();
+        }
+        [HttpPost]
+        [ActionName("SignUp")]  /*Code for binding*/
+        public ActionResult SignUp_Post([Bind(/*Include*/Exclude  = "FirstName, Age, Sex, Email, MobileNum, Password")]User user)
+        {
+            //User user = new User();
+            //TryUpdateModel(user);         
+            //if (ModelState.IsValid)
+            {
+                userRepository.AddUserDetails(user);
+                return RedirectToAction("Index");
+            }
+            //return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
